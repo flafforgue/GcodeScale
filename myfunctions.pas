022c600446059c74db1fe8674184f7fa24f0a48d
flafforgue <30392727+flafforgue@users.noreply.github.com>
2023-03-13 15:02:24 +0100

    Add files via upload

---
+++ myfunctions.pas
@@ -0,0 +1,23 @@
+unit MyFunctions;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+function Len(const S: string) : integer;
+
+
+
+implementation
+
+function Len(const S: string) : integer;
+begin
+end;
+
+
+
+end.
+
